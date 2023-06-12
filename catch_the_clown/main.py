bbcaea5b8ffc2534d8d10208af9fef13749b23bb
ravenusmc <mcuddy77@gmail.com>
2023-06-12 16:22:32 -0400

    basic elements of gameplay added

--- catch_the_clown/main.py
+++ catch_the_clown/main.py
@@ -16,7 +16,7 @@ clock = pygame.time.Clock()
 #Set game value 
 PLAYER_STARTING_LIVES = 5 
 CLOWN_STARTING_VELOCITY = 3
-CLOWN_ACCERLATIO = .5 
+CLOWN_ACCERLATION = .5 
 
 score = 0 
 player_lives = PLAYER_STARTING_LIVES
@@ -68,11 +68,45 @@ clown_rect = clown_image.get_rect()
 clown_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
 #The Main Game Loop 
+pygame.mixer.music.play(-1, 0.0)
 running = True 
 while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
+        
+        #A click is made
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            mouse_x = event.pos[0]
+            mouse_y = event.pos[1]
+
+            #The clown was clicked
+            if clown_rect.collidepoint(mouse_x, mouse_y):
+                click_sound.play()
+                score += 1
+                clown_velocity += CLOWN_ACCERLATION
+                #move clown in new direction
+                previous_dx = clown_dx 
+                previous_dy = clown_dy 
+                while (previous_dx == clown_dx and previous_dy == clown_dy):
+                    clown_dx = random.choice([-1, 1])
+                    clown_dy = random.choice([-1, 1])
+            else:
+                miss_sound.play()
+                player_lives -= 1 
+
+
+    
+    #Move the clown 
+    clown_rect.x += clown_dx * clown_velocity
+    clown_rect.y += clown_dy * clown_velocity
+
+    #Bounce the clown around 
+    if clown_rect.left <= 0 or clown_rect.right >= WINDOW_WIDTH:
+        clown_dx = -1 * clown_dx
+    if clown_rect.top <= 0 or clown_rect.bottom >= WINDOW_HEIGHT:
+        clown_dy = -1 * clown_dy
+
     #Blit The background 
     display_surface.blit(background_image, background_rect)
 
