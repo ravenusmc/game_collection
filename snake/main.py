2efd95f098547420586ce852cb6b1f6c66d34d58
ravenusmc <mcuddy77@gmail.com>
2023-06-18 13:43:57 -0400

    finished snake game

--- snake/main.py
+++ snake/main.py
@@ -11,7 +11,7 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("~SNAKE!~")
 
 #Set FPS and Clock 
-FPS = 20 
+FPS = 5 
 clock = pygame.time.Clock()
 
 #Set game values
@@ -61,7 +61,7 @@ apple_coord = (500,500, SNAKE_SIZE, SNAKE_SIZE)
 apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
 head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
 head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
-body_coord = []
+body_coords = []
 
 #The Main Game lopp
 running = True 
@@ -86,10 +86,38 @@ while running:
                 snake_dx = 0 
                 snake_dy = 1 * SNAKE_SIZE
 
+    #Update head coordinate
+    body_coords.insert(0, head_coord)
+    body_coords.pop()
+
     #Update the x and y position of snakes head 
     head_x += snake_dx
     head_y += snake_dy
     head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+
+    #Check for game over by going off screen
+    if head_rect.left < 0 or head_rect.right > WINDOW_WIDTH or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+
+        #Pause the game 
+        is_paused = True 
+        while is_paused:
+            for event in pygame.event.get():
+                # The player wants to play again 
+                if event.type == pygame.KEYDOWN: 
+                    score = 0 
+                    head_x = WINDOW_WIDTH//2 
+                    head_y = WINDOW_HEIGHT//2
+                    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+                    body_coords = []
+                    snake_dx = 0
+                    snake_dy = 0
+                    is_paused = False 
+                if event.type == pygame.QUIT: 
+                    is_paused = False 
+                    running = False 
 
     #Check for collisions 
     if head_rect.colliderect(apple_rect):
@@ -99,10 +127,11 @@ while running:
         apple_x = random.randint(0, WINDOW_WIDTH - SNAKE_SIZE)
         apple_y = random.randint(0, WINDOW_HEIGHT - SNAKE_SIZE)
         apple_coord = (apple_x, apple_y, SNAKE_SIZE, SNAKE_SIZE)
+
+        body_coords.append(head_coord)
     
     #Update HUD
     score_text = font.render("Score: " + str(score), True, GREEN, DARKRED)
-
 
     #Fill the surface
     display_surface.fill(WHITE)
@@ -112,8 +141,16 @@ while running:
     display_surface.blit(score_text, score_rect)
 
     #blit the assets 
+    for body in body_coords:
+        pygame.draw.rect(display_surface, DARKGREEN, body)
+
     head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
     apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
+
+    #Incrase FPS 
+    # if score != 0 and score % 2 == 0:
+    #     print('HERE')
+    #     FPS += 5
 
     #Update the display 
     pygame.display.update()
