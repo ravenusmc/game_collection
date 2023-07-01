16333ab50eed50779ce2bb4fc537bd048bfa49a3
ravenusmc <mcuddy77@gmail.com>
2023-07-01 11:17:38 -0400

    wrote code for sprites to collide

--- advanced_pygame/two.py
+++ advanced_pygame/two.py
@@ -12,6 +12,37 @@ pygame.display.set_caption("Sprite Groups!")
 FPS = 60
 clock = pygame.time.Clock()
 
+#Player Class 
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, monster_group):
+        super().__init__()
+        self.image = pygame.image.load("./assets/knight.png")
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.velocity = 5
+        self.monster_group = monster_group
+    
+    def update(self):
+        self.move()
+        self.check_collisions()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.velocity
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.velocity
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.velocity
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.velocity
+    
+    def check_collisions(self):
+        if pygame.sprite.spritecollide(self, self.monster_group, True):
+            print(len(self.monster_group))
+
 #Monster class
 class Monster(pygame.sprite.Sprite):
 
@@ -24,12 +55,17 @@ class Monster(pygame.sprite.Sprite):
     
     def update(self):
         self.rect.y += self.velocity
-    
+ 
 #Create a monster group and create 10 monsters
 monster_group = pygame.sprite.Group()
 for i in range(10):
     monster = Monster(i * 64, 10)
     monster_group.add(monster)
+
+#Create a player group and add a player 
+player_group = pygame.sprite.Group()
+player = Player(500,500, monster_group)
+player_group.add(player)
 
 #The main game loop 
 running = True 
@@ -42,6 +78,8 @@ while running:
     display_surface.fill((0,0,0))
 
     #Update and Draw Assets 
+    player_group.update()
+    player_group.draw(display_surface)
     monster_group.update()
     monster_group.draw(display_surface)
     
