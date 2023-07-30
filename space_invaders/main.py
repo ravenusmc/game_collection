4213805284cea9012a728ea1e3da16e0763e03de
ravenusmc <mcuddy77@gmail.com>
2023-07-30 11:49:21 -0400

    started work on player class

--- space_invaders/main.py
+++ space_invaders/main.py
@@ -49,17 +49,34 @@ class Game():
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
-        pass 
+    def __init__(self, bullet_group):
+        super().__init__()
+        self.image = pygame.image.load("./assets/player_ship.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WINDOW_WIDTH//2
+        self.rect.bottom = WINDOW_HEIGHT
+
+        self.lives = 5
+        self.velocity = 8 
+
+        self.bullet_group = bullet_group
+        self.shoot_sound = pygame.mixer.Sound("./assets/player_fire.wav")
+
 
     def update(self):
-        pass 
+        keys = pygame.key.get_pressed()
+
+        #Move the player within the bounds of the screen 
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.velocity
+        if keys[pygame.K_RIGHT] and self.rect.right < WINDOW_WIDTH:
+            self.rect.x += self.velocity
 
     def fire(self):
         pass 
 
     def reset(self):
-        pass 
+        self.rect.centerx = WINDOW_WIDTH//2
 
 class Alien(pygame.sprite.Sprite):
 
@@ -93,7 +110,18 @@ class AlienBullet(pygame.sprite.Sprite):
 
 #Create Bullet groups 
 my_player_bullet_group = pygame.sprite.Group()
+my_alien_bullet_group = pygame.sprite.Group() 
 
+#Create a player group and player object 
+my_player_group = pygame.sprite.Group()
+my_player = Player(my_player_bullet_group)
+my_player_group.add(my_player)
+
+#Create an alien group 
+my_alien_group = pygame.sprite.Group()
+
+#Create a game object
+my_game = Game()
 
 #Main Game Loop 
 running = True 
@@ -101,6 +129,25 @@ while running:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False 
+    
+    #Fill the display 
+    display_surface.fill((0,0,0))
+
+    #Update and display all sprites
+    my_player_group.update() 
+    my_player_group.draw(display_surface)
+
+    my_alien_group.update()
+    my_alien_group.draw(display_surface)
+
+    my_player_bullet_group.update()
+    my_player_bullet_group.draw(display_surface)
+
+    my_alien_bullet_group.update() 
+    my_alien_bullet_group.draw(display_surface)
+
+    my_game.update()
+    # my_game.draw(display_surface)
         
 
     pygame.display.update()
