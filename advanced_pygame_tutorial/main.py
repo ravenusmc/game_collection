ba67714a53350027d0acd250afed31b61ed7e478
ravenusmc <mcuddy77@gmail.com>
2023-08-30 16:39:19 -0400

    added ability to jump and wrap around movement

--- advanced_pygame_tutorial/main.py
+++ advanced_pygame_tutorial/main.py
@@ -1,37 +1,39 @@
-import pygame 
+import pygame
 vector = pygame.math.Vector2
 
-#init pygame 
-pygame.init() 
+# init pygame
+pygame.init()
 
-#set display surface (Tile size is 32X32 so 960/32 = 30 tiles wide, 64/32 = 20 tiles high)
+# set display surface (Tile size is 32X32 so 960/32 = 30 tiles wide, 64/32 = 20 tiles high)
 WINDOW_WIDTH = 960
-WINDOW_HEIGHT = 640 
+WINDOW_HEIGHT = 640
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Making a Tile Map!")
 
-#Set FPS and clock 
-FPS = 60 
+# Set FPS and clock
+FPS = 60
 clock = pygame.time.Clock()
 
-#Classes 
+# Classes
+
+
 class Tile(pygame.sprite.Sprite):
 
     def __init__(self, x, y, image_int, main_group, sub_group=""):
         super().__init__()
-        if image_int == 1: 
+        if image_int == 1:
             self.image = pygame.image.load("./assets/dirt.png")
-        elif image_int == 2: 
+        elif image_int == 2:
             self.image = pygame.image.load("./assets/grass.png")
             sub_group.add(self)
         elif image_int == 3:
             self.image = pygame.image.load("./assets/water.png")
             sub_group.add(self)
-        #add every tile 
+        # add every tile
         main_group.add(self)
 
-        self.rect = self.image.get_rect() 
-        self.rect.topleft = (x,y)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -43,110 +45,156 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.bottomleft = (x, y)
 
+        self.starting_x = x
+        self.starting_y = y
         self.grass_tiles = grass_tiles
         self.water_tiles = water_tiles
 
-        #Kinematics vectors (first value is the x, second value is the y)
+        # Kinematics vectors (first value is the x, second value is the y)
         self.position = vector(x, y)
         self.velocity = vector(0, 0)
         self.acceleration = vector(0, 0)
 
-        #Kinematic constants
+        # Kinematic constants
         self.HORIZONTAL_ACCELERATION = 2
         self.HORIZONTAL_FRICTION = 0.15
-        self.VERTICAL_ACCLERATION = .5 #Gravity
+        self.VERTICAL_ACCLERATION = .5  # Gravity
+        self.VERTICAL_JUMP_SPEED = 15  # determines how high can jump
 
     def update(self):
+        self.move()
+        self.check_collisions()
+
+    def move(self):
         self.acceleration = vector(0, self.VERTICAL_ACCLERATION)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.acceleration.x = -1 * self.HORIZONTAL_ACCELERATION
         if keys[pygame.K_RIGHT]:
-            self.acceleration.x = self.HORIZONTAL_ACCELERATION 
+            self.acceleration.x = self.HORIZONTAL_ACCELERATION
         self.acceleration.x -= self.velocity.x * self.HORIZONTAL_FRICTION
         self.velocity += self.acceleration
         self.position += self.velocity + 0.5 * self.acceleration
-        #update new rect based on calculations
-        self.rect.bottomleft = self.position
 
-        #Check for collisions 
-        collided_platforms = pygame.sprite.spritecollide(self, self.grass_tiles, False)
+        # update new rect based on calculations and add wrap around motion
+        if self.position.x < 0:
+            self.position.x = WINDOW_WIDTH
+        elif self.position.x > WINDOW_WIDTH:
+            self.position.x = 0
+        self.rect.bottomleft = self.position
+    
+    def check_collisions(self):
+        # Check for collisions
+        collided_platforms = pygame.sprite.spritecollide(
+            self, self.grass_tiles, False)
         if collided_platforms:
-            self.position.y = collided_platforms[0].rect.top + 1
-            self.velocity.y = 0
-        #Check with collisions with water 
+            if self.velocity.y > 0: 
+                self.position.y = collided_platforms[0].rect.top + 1
+                self.velocity.y = 0
+        # Check with collisions with water
         if pygame.sprite.spritecollide(self, self.water_tiles, False):
             print("You can't Swim!")
+            self.position = vector(self.starting_x, self.starting_y)
+            self.velocity = vector(0, 0)
+
+    def jump(self):
+        # only jump if on a grass tile
+        if pygame.sprite.spritecollide(self, self.grass_tiles, False):
+            self.velocity.y = self.VERTICAL_JUMP_SPEED * -1
 
 
-#Create Sprite group 
+# Create Sprite group
 main_tile_group = pygame.sprite.Group()
 grass_tile_group = pygame.sprite.Group()
 water_tile_group = pygame.sprite.Group()
 my_player_group = pygame.sprite.Group()
 
-#Create the tile map 0 -> no tile, 1 -> dirt, 2 - grass 3 ->water
-#20 rows 30 columns 
+# Create the tile map 0 -> no tile, 1 -> dirt, 2 - grass 3 ->water
+# 20 rows 30 columns
 tile_map = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2],
-    [0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,4,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2],
-    [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1]
 ]
 
-#create individual tile object from the tile map 
+# create individual tile object from the tile map
 for i in range(len(tile_map)):
     for j in range(len(tile_map[i])):
-        if tile_map[i][j] == 1: 
+        if tile_map[i][j] == 1:
             Tile(j*32, i*32, 1, main_tile_group)
         elif tile_map[i][j] == 2:
             Tile(j*32, i*32, 2, main_tile_group, grass_tile_group)
         elif tile_map[i][j] == 3:
             Tile(j*32, i*32, 3, main_tile_group, water_tile_group)
         elif tile_map[i][j] == 4:
-            my_player = Player(j*32, i*32 + 32, grass_tile_group, water_tile_group)
+            my_player = Player(
+                j*32, i*32 + 32, grass_tile_group, water_tile_group)
             my_player_group.add(my_player)
 
-#Load background image 
-background_image = pygame.image.load("./assets/background.png")   
+# Load background image
+background_image = pygame.image.load("./assets/background.png")
 background_rect = background_image.get_rect()
-background_rect.topleft = (0,0)
+background_rect.topleft = (0, 0)
 
-#Main game loop 
-running = True 
-while running: 
+# Main game loop
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
-    
-    #Blit the background 
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                my_player.jump()
+
+    # Blit the background
     display_surface.blit(background_image, background_rect)
 
-    #Draw the tiles 
+    # Draw the tiles
     main_tile_group.draw(display_surface)
 
-    #update and draw sprites 
+    # update and draw sprites
     my_player_group.update()
     my_player_group.draw(display_surface)
-    
-    pygame.display.update() 
+
+    pygame.display.update()
     clock.tick(FPS)
 
-#End the game 
-pygame.quit() 
+# End the game
+pygame.quit()
